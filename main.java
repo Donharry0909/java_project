5bf358aadd117ced08addbc4844132f6ccbfe902
Donharry0909 <harry30701a@gmail.com>
2024-05-23 16:00:03 +0800

    test

--- main.java
+++ main.java
@@ -1,6 +1,10 @@
-public class main {
+import javax.swing.JFrame;
 
+public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Frame frame = new Frame();
+        frame.setSize(400, 300);   //設定寬，長
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //設定預設的關閉視窗
+        frame.setVisible(true);    //視窗預設是不可見的
     }
 }
